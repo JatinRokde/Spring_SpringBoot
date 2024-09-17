7765387c9a5d7b5fe1f1b67b71abf3e1e917a9cc
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-09-18 01:04:18 +0530

    Spring Boot Rest - Read

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/jr/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/jr/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.jr.springboot.cruddemo.entity.Employee;
 import com.jr.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee ID not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
