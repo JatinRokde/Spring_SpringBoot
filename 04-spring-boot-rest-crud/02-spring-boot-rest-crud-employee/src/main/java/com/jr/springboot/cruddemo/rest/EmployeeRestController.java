279f1926dafd38a41802664454938a0c4cc10830
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-09-18 01:29:54 +0530

    Spring Boot Rest - Create

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/jr/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/jr/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,10 +4,7 @@ import com.jr.springboot.cruddemo.dao.EmployeeDAO;
 import com.jr.springboot.cruddemo.entity.Employee;
 import com.jr.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +35,17 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
+    }
+
+    // add mapping for POST /employees - add a new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        // also just in case they pass the id in JSON.. set id to 0
+        // this is to force the save of a new item.. instead of update
+
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
     }
 }
