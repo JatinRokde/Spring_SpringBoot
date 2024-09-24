b63eb6c04ff596af2191513f0b9c2625ea55f1ce
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-09-24 19:47:10 +0530

    Spring MVC - Min Max Validation

--- 06-spring-boot-spring-mvc/02-validation-demo/src/main/java/com/jr/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-validation-demo/src/main/java/com/jr/springdemo/mvc/Customer.java
@@ -12,6 +12,18 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName = "";
 
+    @Min(value = 0, message = "Must be greater than or equal to 0")
+    @Max(value = 10, message = "Must be lesser than or equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
     public String getFirstName() {
         return firstName;
     }
