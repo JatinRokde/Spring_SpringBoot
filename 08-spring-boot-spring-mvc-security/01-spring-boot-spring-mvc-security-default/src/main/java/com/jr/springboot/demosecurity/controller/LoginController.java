aa8b1754e4de700e05a79e6f0ce3dceab68278ae
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-01 19:46:40 +0530

    Spring MVC Security - Custom Access Denied Page

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/jr/springboot/demosecurity/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/jr/springboot/demosecurity/controller/LoginController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping("showMyLoginPage")
+    @GetMapping("/showMyLoginPage")
     public String showMyLoginPage() {
         return "fancy-login";
+    }
+
+    // add request for access-denied
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
     }
 }
