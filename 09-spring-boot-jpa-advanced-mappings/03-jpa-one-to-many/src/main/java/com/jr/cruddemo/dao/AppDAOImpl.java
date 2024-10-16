f7bd5f3bc01912cafb41f83186b12fe2fcc06c28
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-17 00:21:15 +0530

    JPA Hibernate Advanced Mapping - Delete Instructor

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/jr/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/jr/cruddemo/dao/AppDAOImpl.java
@@ -39,9 +39,16 @@ public class AppDAOImpl implements AppDAO {
         // retrieve the instructor
         Instructor tempInstructor = entityManager.find(Instructor.class, theId);
 
+        // get the courses
+        List<Course> courses = tempInstructor.getCourses();
+
+        // break the association of all courses for the instructor
+        for (Course tempCourse : courses) {
+            tempCourse.setInstructor(null);
+        }
+
         // delete the instructor
         entityManager.remove(tempInstructor);
-
     }
 
     @Override
