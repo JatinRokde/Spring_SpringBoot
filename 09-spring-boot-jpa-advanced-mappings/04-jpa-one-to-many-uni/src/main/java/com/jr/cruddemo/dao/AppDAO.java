20e83d2ebe55495447100eae04119ec74e0ccd8d
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-17 01:23:12 +0530

    JPA Hibernate Advanced Mapping - Create Review Entity

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/jr/cruddemo/dao/AppDAO.java
@@ -0,0 +1,31 @@
+package com.jr.cruddemo.dao;
+
+import com.jr.cruddemo.entity.Course;
+import com.jr.cruddemo.entity.Instructor;
+import com.jr.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save (Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+}
