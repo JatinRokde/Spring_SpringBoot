3548e2601b94226108da40706370f22d31c34840
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-18 22:38:53 +0530

    AOP - Pointcut match method with Account and more params

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/jr/aopdemo/dao/AccountDAO.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/jr/aopdemo/dao/AccountDAO.java
@@ -3,5 +3,5 @@ package com.jr.aopdemo.dao;
 import com.jr.aopdemo.Account;
 
 public interface AccountDAO {
-    void addAccount(Account theAccount);
+    void addAccount(Account theAccount, boolean vipFlag);
 }
