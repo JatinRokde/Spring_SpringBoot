0943a18bd3877ea6198987836359143735598fb0
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-18 21:34:54 +0530

    AOP - Pointcut match on any return type

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/jr/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/jr/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
 
-    @Before("execution(void add*())")
+    @Before("execution(* add*())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>>>> Executing @Before advice on addAccount()");
     }
