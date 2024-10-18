4fbfa7587b5e7e177a4b66200f0a3403d47dfa64
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-18 18:04:49 +0530

    AOP - Before Advice

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/jr/aopdemo/AopdemoApplication.java
@@ -0,0 +1,30 @@
+package com.jr.aopdemo;
+
+import com.jr.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+		// call the business method
+		theAccountDAO.addAccount();
+
+		// call the business method again
+		theAccountDAO.addAccount();
+	}
+}
---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/jr/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,5 @@
+package com.jr.aopdemo.dao;
+
+public interface AccountDAO {
+    void addAccount();
+}
