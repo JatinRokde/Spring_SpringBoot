2a15d6047332b14ac35727014433e9e8b1acd9b6
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2024-10-22 01:44:53 +0530

    AOP - Integrate AOP with Spring MVC Crud app

---
+++ 10-spring-boot-aop/03-spring-boot-aop-mvc-crud-demo-employees/src/main/java/com/jr/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.jr.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.jr.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
